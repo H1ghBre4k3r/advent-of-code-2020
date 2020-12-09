1baf5e62214aaf370af890834eb4d0e62aadfe02
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2020-12-09 14:32:38 +0100

    Shorter day 3 even more

--- day_03/solution.js
+++ day_03/solution.js
@@ -1,4 +1,4 @@
 // Task 1
 a=document.getElementsByTagName("pre")[c=0].innerHTML.split("\n");for(i in a){c+=a[i][(i*3)%a[i].length]=="#"}
 // Task 2
-a=document.getElementsByTagName("pre")[0].innerHTML.split("\n");[[r=1,1],[1,3],[1,5],[1,7],[2,0.5]].forEach(e=>{c=i=0;for(;i in a;i+=e[0]){c+=a[i][(i*e[1])%a[i].length]=="#"}r*=c})
+a=document.getElementsByTagName("pre")[0].innerHTML.split("\n");[[r=1,1],[1,3],[1,5],[1,7],[2,0.5]].forEach(([x,y])=>{c=i=0;for(;a[i];i+=x){c+=a[i][(i*y)%a[i].length]=="#"}r*=c})
